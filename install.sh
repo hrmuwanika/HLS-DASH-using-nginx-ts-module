d032a2e358f3a0e30280a2af664ac7f21026ceb1
Henry Robert Muwanika <hrmuwanika@gmail.com>
2021-02-28 10:19:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -79,7 +79,8 @@ http {
             ts;
             ts_hls path=/var/media/hls segment=10s;
             ts_dash path=/var/media/dash segment=10s;
-
+            
+            # This directive sets unlimited request body size
             client_max_body_size 0;
         }
 
